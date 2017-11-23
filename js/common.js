ac6b7342b6e055b0a6b99fa4191a5d87b87fb4ce
liji <1111@qq.com>
2017-11-23 16:58:50 +0800

    index.html完成,user.html开始

--- js/common.js
+++ js/common.js
@@ -7,5 +7,51 @@ $(function(){
   $(document).ajaxStop(function(){
     NProgress.done();
   })
+
+  if(location.href.indexOf('login.html')==-1){
+    $.ajax({
+      type:'get',
+      url:'/employee/checkRootLogin',
+      success:function(info){
+        if(info.error ==400){
+          location.href="login.html"
+
+        }
+
+      }
+    })
+
+  }
+
+
+  $('.category_next').prev().on('click',function(){
+    $(this).next().stop().slideToggle();
+  })
+
+  $('.icon_left').on('click',function(){
+    $('#aslide-left').toggleClass('active');
+    $('#content-right').toggleClass('active');
+  })
+
+  $('.icon_right').on('click',function(){
+    $('#modal_logout').modal('show');
+
+    $('.btn_logout').off().on('click',function(){
+      $.ajax({
+        type:'get',
+        url:'/employee/employeeLogout',
+        success:function(info){
+          if(info.success){
+            location.href="login.html"
+
+          }
+
+        }
+      })
+    })
+  })
+
+
+
   
 })
